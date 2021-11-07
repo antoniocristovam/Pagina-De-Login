4adf28deffb9eba342f5a0dcfb65d36dd5969b4d
Antonio Cristovam <84741807+antoniocristovam@users.noreply.github.com>
2021-11-07 18:07:39 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+document.getElementById('olhinho').addEventListener('mousedown', function() {
+    document.getElementById('senha').type = 'text';
+  });
+  
+  document.getElementById('olhinho').addEventListener('mouseup', function() {
+    document.getElementById('senha').type = 'password';
+  });
+  
+  document.getElementById('olhinho').addEventListener('mousemove', function() {
+    document.getElementById('senha').type = 'password';
+  });
