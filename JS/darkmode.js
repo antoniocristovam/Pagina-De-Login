057c5f16eddca1d0b62d57d6380b9b08bde56f8a
Antonio Cristovam <84741807+antoniocristovam@users.noreply.github.com>
2021-11-08 16:19:16 -0300

    Delete JS directory

--- JS/darkmode.js
+++
@@ -1,7 +0,0 @@
-    const btn = document.querySelector('.btn');
-    const container = document.querySelector('.container');
-    btn.onclick = function()
-{
-    this.classList.toggle('active')
-    container.classList.toggle('active')
-}
